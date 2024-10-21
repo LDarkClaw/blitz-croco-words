35f64652addc1e022ee18e233b084861fbfe1b06
LDarkClaw <153105341+LDarkClaw@users.noreply.github.com>
2024-10-21 20:27:05 +0300

    06

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import os
+from pathlib import Path
+import zipfile
 
-current_file = os.path.realpath(__file__)
-current_directory = os.path.dirname(current_file)
+def ad():
+    current_file = os.path.realpath(__file__)
+    current_directory = os.path.dirname(current_file)
 
-print(current_file)
+    return current_directory
+
+zip_file_name='croco-blitz-source.zip'
+def print_zip():
+    with zipfile.ZipFile(Path(ad()) / 'src' / zip_file_name,'r') as zip_file:
+        file = zip_file.namelist()
+    print("список файлов")
+    for f in file:
+        print(f)
+
+if __name__=='__main__':
+    print_zip()
